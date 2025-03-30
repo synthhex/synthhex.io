b6da727abaaa5d990c72c010d1fe08a4a939fe36
Valerie-June Kamberova <hexadecimal.overflow@gmail.com>
2025-03-31 01:48:20 +0200

    feat: mvp finished

---
+++ README.md
@@ -0,0 +1,2 @@
+# Synthhex.io
+My personal website's code hosted at synthhex.io.
---
+++ website/src/components/blurbs/types.ts
@@ -0,0 +1,38 @@
+export type Fallback = {
+    title: string;
+    description: string;
+    link: string;
+}
+
+export interface IBlurb<T extends string = never> {
+    type: T;
+    fallback?: Fallback;
+}
+
+export const defaultFallback: Fallback = {
+    title: 'No title available',
+    description: 'No description available',
+    link: 'google.com'
+}
+
+
+export interface IGithubBlurb extends IBlurb<'github'> {
+    repo: string;
+    useGithubDescription?: boolean;
+}
+
+export interface IDocsBlurb extends IBlurb<'docs'> {
+    title: string;
+    description: string;
+    link: string;
+}
+
+
+export interface ITextBlurb extends IBlurb<'text'> {
+    title: string;
+    description: string;
+    link?: string;
+    image?: string;
+}
+
+export type Blurb = IGithubBlurb | IDocsBlurb | ITextBlurb;
